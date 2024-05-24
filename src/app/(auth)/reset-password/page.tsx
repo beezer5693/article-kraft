2b3aacca5af29b363511ff7ffad5152a4d459643
Brandon Bryan <bshwag@gmail.com>
2024-05-24 18:05:11 -0400

    Fixed param issue

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -4,21 +4,15 @@ import FormContainer from "@/components/auth/FormContainer";
 import FormHeader from "@/components/auth/FormHeader";
 import ResetPasswordForm from "@/components/auth/ResetPasswordForm";
 
-const ResetPassword = ({
-    searchParams,
-}: {
-    searchParams: { code: string; error: string; message: string };
-}) => {
-    const { code, error, message } = searchParams;
-
-    console.log(code);
+const ResetPassword = ({ searchParams }: { searchParams: { error: string; message: string } }) => {
+    const { error, message } = searchParams;
 
     return (
         <AuthPageContainer>
             {error && <AuthMessage error={error === "true"} message={message} />}
             <FormContainer>
                 <FormHeader variant={"RESET_PASSWORD"} />
-                <ResetPasswordForm code={code} />
+                <ResetPasswordForm />
             </FormContainer>
         </AuthPageContainer>
     );
