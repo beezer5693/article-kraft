9f792750dcccc2e6a1df34a376a808dc8cdd0145
Brandon Bryan <bshwag@gmail.com>
2024-05-24 18:03:08 -0400

    Removed unused files

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <h1 className="text-3xl font-medium">Article Kraft</h1>
-            <p>Enhance your article's readability and SEO by using our AI-powered tool.</p>
+            <p>Enhance your article&apos;s readability and SEO by using our AI-powered tool.</p>
             <div className="flex items-center gap-3 mt-5">
                 <Link href="/auth/signup">
                     <Button>Sign up</Button>
